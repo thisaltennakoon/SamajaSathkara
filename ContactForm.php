d728fcc1cd5fc46b51963d6fccbfe1c3afddc5d8
thisaltennakoon <42693357+thisaltennakoon@users.noreply.github.com>
2019-04-25 19:16:00 +0530

    edit

--- ContactForm.php
+++ ContactForm.php
@@ -42,7 +42,8 @@ input[type=submit]:hover {
 <h3>Contact Form</h3>
 
 <div class="container">
-  <form action="hello.php" method="post" />
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+  <!--form action="hello.php" method="post" /-->
 
     <label for="fname">First Name</label>
     <input type="text" id="fname" name="firstname" placeholder="Kumar">
@@ -68,3 +69,54 @@ input[type=submit]:hover {
 
 </body>
 </html>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+$firstname=$_POST['firstname'];
+echo "Your First Name: ".$firstname."<br>";
+$lastname=$_POST['lastname'];
+echo "Your Last Name: ".$lastname."<br>";
+$email=$_POST['email'];
+echo "Your Emali Address: ".$email."<br>";
+$pnumber=$_POST['pnumber'];
+echo "Your Phone Number: ".$pnumber."<br>";
+$message=$_POST['message'];
+echo "Your Message is : ".$message."<br>";
+
+$servername = "localhost";                                    // host which has established our computer.If the database is on a sever, then this should change accordinly
+$username = "root";                                           // username and the password of the mysql sever
+$password = "";
+$dbname = "SamajaSathkara";
+$conn = new mysqli($servername,$username,$password,$dbname); // making the connection with mysql
+if ($conn->connect_error){                                   // check whether the connection is correctly established or not
+    die("Connection failed: " . $conn->connect_error);
+}                                                            // upto here ,connection is established
+
+
+
+$c="'";
+$date=strval(date("Y-m-d"));
+$time= strval(date("H:i:s"));
+
+$sql = "INSERT INTO ContactForm(readed,submitdate,submittime,firstname,lastname,email,pnumber,messages) VALUES (false,'$date','$time','$firstname','$lastname','$email','$pnumber','$message')"; // insert data to the created table
+if ($conn->query($sql)===TRUE){
+    echo "<h3>Your Contact Form has been successfully submitted.</h3>"."<h3>We will contact you soon..</h3>";
+}else{
+    echo "Error: ". $sql ."<br>" . $conn->error;
+}
+
+
+
+
+/*$sql = "SELECT firstname,lastname,email,pnumber FROM ContactForm"; //reading things from the table
+$result = $conn->query($sql);
+if ($result->num_rows > 0){
+    while($row = $result->fetch_assoc()){       //while loop
+        echo "firstname: ". $row["firstname"]."<br>"." lastname: " . $row["lastname"]."<br>"."email: ".$row["email"]."<br>"."pnumber: ".$row["pnumber"]."<br>"."<br>";
+    }                       
+}else{
+    echo "0 results";
+}*/
+
+$conn->close();    //close the connection with database
+}
+?>
--- ProposeProject.php
+++ ProposeProject.php
@@ -42,7 +42,8 @@ input[type=submit]:hover {
 <h3>Propose a project</h3>
 
 <div class="container">
-  <form action="sendToProposedProject.php" method="post" />
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+  <!--form action="sendToProposedProject.php" method="post" /-->
 
     <label for="fname">Your Name</label>
     <input type="text" id="name" name="name" placeholder="Kumar">
@@ -67,3 +68,33 @@ input[type=submit]:hover {
 
 </body>
 </html>
+
+<?php
+$servername = "localhost"; 
+$username = "root";                                            
+$password = "";
+$dbname = "SamajaSathkara";
+$conn = new mysqli($servername,$username,$password,$dbname); // making the connection with mysql
+if ($conn->connect_error){                       
+    die("Connection failed: " . $conn->connect_error);
+}                                                           
+$name=$_POST['name'];
+echo "Name: ".$name."<br>";
+$pnumber=$_POST['pnumber'];
+echo "Your Phone Number: ".$pnumber."<br>";
+$email=$_POST['email'];
+echo "Your Emali Address: ".$email."<br>";
+$description=$_POST['description'];
+echo "Description: ".$description."<br>";
+
+
+
+$sql = "INSERT INTO proposedprojects(proposername,pnumber,email,description) VALUES ('$name','$pnumber','$email','$description')";
+if ($conn->query($sql)===TRUE){
+    echo "<h3>The project you proposed has been recorded successfully</h3>"."<h3>We will contact you soon..</h3>";
+}else{
+    echo "Error: ". $sql ."<br>" . $conn->error;
+}
+
+    
+?>
