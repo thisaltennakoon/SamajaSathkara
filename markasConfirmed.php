5f637d151653a0b3eaf38623794b9f3ede0f9500
thisaltennakoon <42693357+thisaltennakoon@users.noreply.github.com>
2019-05-27 17:27:17 +0530

    mark submitted donations as confirmed

--- markasConfirmed.php
+++ markasConfirmed.php
@@ -1,5 +1,5 @@
 <?php
-$read="3";//$_POST['read'];
+$read=$_POST['read'];
 
 $servername = "localhost";                                    // host which has established our computer.If the database is on a sever, then this should change accordinly
 $username = "root";                                           // username and the password of the mysql sever
@@ -20,7 +20,7 @@ if ($result->num_rows > 0){
     }
 }
 
-$sql = 'SELECT completed,estimatedprojectcost,raised FROM Projects Where id='.$projectid; //reading things from the table
+$sql = 'SELECT completed,estimatedprojectcost,raised FROM Projects Where projectid="'.$projectid.'"'; //reading things from the table
 $result = $conn->query($sql);
 if ($result->num_rows > 0){
     while($row = $result->fetch_assoc()){
@@ -29,13 +29,23 @@ if ($result->num_rows > 0){
         $raised=$row["raised"];
     }
 }
-$newraised=$raised+$amount;
-if ($estimatedprojectcost>=$newraised){
-    $newcompleted=1;
+
+echo 'completed='.$completed;
+echo '<br>';
+echo 'estimatedprojectcost='.$estimatedprojectcost;
+echo '<br>';
+echo 'raised='.$raised;echo '<br>';
+
+$raised=$raised+$amount;
+if ($estimatedprojectcost<=$raised){
+    $completed=1;
 }
-$sql ='UPDATE Projects SET raised='.$newraised.' WHERE id='.$projectid;
+echo 'newraised='.$raised;echo '<br>';
+echo 'new completed='.$completed;
+
+$sql ='UPDATE Projects SET raised='.$raised.' Where projectid="'.$projectid.'"';
 $conn->query($sql);
-$sql ='UPDATE Projects SET completed='.$newcompleted.' WHERE id='.$projectid;
+$sql ='UPDATE Projects SET completed='.$completed.' Where projectid="'.$projectid.'"';
 $conn->query($sql);
 $sql ='UPDATE Donation SET approved=true WHERE id='.$read;
 $conn->query($sql);
